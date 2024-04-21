a85c3558af1544cc4e73ef0b19025138578b0d78
Hari Haran N <110599490+Hareesvar7@users.noreply.github.com>
2024-04-22 00:14:50 +0530

    Create Write a function to check if a linked list contains a cycle.js

---
+++ Write a function to check if a linked list contains a cycle.js
@@ -0,0 +1,20 @@
+const hasCycle = (head) => {
+    // Set variable to store distinct nodes
+    let set = new Set()
+    // for traversing, initialize current with head node
+    let current = head
+    
+    //traverse the linked list
+    while(current){
+        if(set.has(current)){
+            // if duplication occurs, return true
+            return true
+        }else{
+            set.add(current)
+        }
+        
+        current = current.next
+    }
+    // traverse is completed, cycle not found
+    return false
+};
